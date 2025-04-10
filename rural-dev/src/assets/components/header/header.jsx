d4ccfbfdb5497fbc211158b8c174fde22bccedf0
anubhab1001 <deanubhab555@gmail.com>
2025-04-11 01:34:51 +0530

    Merged upstream with local preference in conflicts

--- rural-dev/src/assets/components/header/header.jsx
+++ rural-dev/src/assets/components/header/header.jsx
@@ -134,7 +134,7 @@ export default function Header() {
             >
               <MenuItem>
                 <a
-                  href="#"
+                  href="/profile"
                   className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                 >
                   Your Profile
